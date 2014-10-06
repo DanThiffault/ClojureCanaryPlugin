d59c14b5b41ed83642d07ac89cdb4aa39961c6ff
Dan Thiffault <dan@119labs.com>
2014-10-06 18:44:00 +0530

    Added remote repo for deps

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :repositories {"vi-repo" "http://repo.visualillusionsent.net/repository/public/"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [net.canarymod/CanaryMod "1.7.10-1.1.2"]]
+  :aot :all)
