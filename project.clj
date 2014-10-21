12e122531c3cbbd1a8ba1bf33549fa1c1a8d63e8
Dan Thiffault <dan@119labs.com>
2014-10-22 00:18:33 +0530

    Added ability to connect via nrepl

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"vi-repo" "http://repo.visualillusionsent.net/repository/public/"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [net.canarymod/CanaryLib "1.1.3-SNAPSHOT"]])
+                 [net.canarymod/CanaryLib "1.1.3-SNAPSHOT"]
+                 [org.clojure/tools.nrepl "0.2.5"]])
--- src/clj_canary_plug/core.clj
+++ src/clj_canary_plug/core.clj
@@ -1,9 +1,20 @@
 (ns clj-canary-plug.core
+  (:use [clojure.tools.nrepl.server :only (start-server stop-server)])
   (:import [net.canarymod.plugin.Priority]
            [net.canarymod.logger.Logman]))
 
-(defn handlePlayerConnected [hook] 
+(defn handlePlayerConnected [hook]
   (.info (net.canarymod.logger.Logman/getLogman "ClojurePlugin") "new player connected"))
 
+(def config [["net.canarymod.hook.player.ConnectionHook" 'clj-canary-plug.core/handlePlayerConnected  net.canarymod.plugin.Priority/NORMAL]])
+
+(defn lookup-handler [sym]
+  (fn [& args] (apply (resolve sym) args)))
+
+(defn register [plugin]
+  (fn [[hookName handlerSym priority]]
+    (.registerHook plugin hookName (lookup-handler handlerSym) priority)))
+
 (defn enablePlugin [plugin]
- (.registerHook plugin "net.canarymod.hook.player.ConnectionHook" handlePlayerConnected net.canarymod.plugin.Priority/NORMAL))
+  (doall (map (register plugin) config))
+  (defonce server (start-server :port 7888)))
