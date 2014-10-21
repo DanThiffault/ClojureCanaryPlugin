2d2a74a453f69295252ce21ca54f1b289d84ee8b
Dan Thiffault <dan@119labs.com>
2014-10-21 21:02:00 +0530

    Working basic plugin

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject clj-canary-plug "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://119labs.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"vi-repo" "http://repo.visualillusionsent.net/repository/public/"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [net.canarymod/CanaryMod "1.7.10-1.1.2"]]
-  :aot :all)
+                 [net.canarymod/CanaryLib "1.1.3-SNAPSHOT"]])
--- src/clj_canary_plug/core.clj
+++ src/clj_canary_plug/core.clj
@@ -1,6 +1,9 @@
-(ns clj-canary-plug.core)
+(ns clj-canary-plug.core
+  (:import [net.canarymod.plugin.Priority]
+           [net.canarymod.logger.Logman]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn handlePlayerConnected [hook] 
+  (.info (net.canarymod.logger.Logman/getLogman "ClojurePlugin") "new player connected"))
+
+(defn enablePlugin [plugin]
+ (.registerHook plugin "net.canarymod.hook.player.ConnectionHook" handlePlayerConnected net.canarymod.plugin.Priority/NORMAL))
