c5ca19ee7546de6cf756839982ca3c7f2ad09018
Dan Thiffault <dan@119labs.com>
2014-10-22 09:50:06 +0530

    Factored out common code

--- src/clj_canary_plug/core.clj
+++ src/clj_canary_plug/core.clj
@@ -1,20 +1,16 @@
 (ns clj-canary-plug.core
-  (:use [clojure.tools.nrepl.server :only (start-server stop-server)])
+  (:require [net.canarymod.plugin.clojure.core :refer [registerAll log]])
   (:import [net.canarymod.plugin.Priority]
            [net.canarymod.logger.Logman]))
 
-(defn handlePlayerConnected [hook]
-  (.info (net.canarymod.logger.Logman/getLogman "ClojurePlugin") "new player connected"))
-
-(def config [["net.canarymod.hook.player.ConnectionHook" 'clj-canary-plug.core/handlePlayerConnected  net.canarymod.plugin.Priority/NORMAL]])
-
-(defn lookup-handler [sym]
-  (fn [& args] (apply (resolve sym) args)))
-
-(defn register [plugin]
-  (fn [[hookName handlerSym priority]]
-    (.registerHook plugin hookName (lookup-handler handlerSym) priority)))
+(def config { 
+             :hooks [["net.canarymod.hook.player.ConnectionHook" 'clj-canary-plug.core/handlePlayerConnected  net.canarymod.plugin.Priority/NORMAL]]
+             :repl-port 7888
+             })
 
 (defn enablePlugin [plugin]
-  (doall (map (register plugin) config))
-  (defonce server (start-server :port 7888)))
+  (registerAll plugin config))
+
+(defn handlePlayerConnected [hook]
+  (log :info "New player connected"))
+
