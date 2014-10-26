e52ea2248423cc4b4cf367baa4b2d2cb6e17cb3f
Dan Thiffault <dan@119labs.com>
2014-10-26 15:19:37 +0530

    Added commands & repl control

--- src/clj_canary_plug/core.clj
+++ src/clj_canary_plug/core.clj
@@ -1,16 +1,26 @@
 (ns clj-canary-plug.core
-  (:require [net.canarymod.plugin.clojure.core :refer [registerAll log]])
+  (:require [net.canarymod.plugin.clojure.core :refer [registerAll log] :as canary])
   (:import [net.canarymod.plugin.Priority]
            [net.canarymod.logger.Logman]))
 
 (def config { 
              :hooks [["net.canarymod.hook.player.ConnectionHook" 'clj-canary-plug.core/handlePlayerConnected  net.canarymod.plugin.Priority/NORMAL]]
+             :commands [[{:aliases ["foo.startrepl"]} 'clj-canary-plug.core/start-repl false]
+                        [{:aliases ["foo.stoprepl"]} 'clj-canary-plug.core/stop-repl false]]
              :repl-port 7888
              })
 
-(defn enablePlugin [plugin]
-  (registerAll plugin config))
+(def plugin (ref nil)) 
+
+(defn enablePlugin [p]
+  (dosync (ref-set plugin p))
+  (registerAll @plugin config))
 
 (defn handlePlayerConnected [hook]
-  (log :info "New player connected"))
+  (log :info "new player connected"))
 
+(defn start-repl 
+  ([name] (start-repl name (:repl-port config)))
+  ([name port] (canary/start-repl @plugin port)))
+
+(defn stop-repl [name] (canary/stop-repl @plugin))
